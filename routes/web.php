e6de45ed31662edfc49cc2cac2c990f20432976d
paola <angie.paola.medina@correounivalle.edu.co>
2020-12-19 13:37:37 -0500

    conexion API OMDB

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('getMoviesOMDB', 'App\Http\Controllers\OMDBController@getMovieCollection');
